ca7bc6ffb0fead9eeea0d9f8e61cfaac34d09297
Screen117 <j_goom@yahoo.com>
2024-09-26 11:34:14 -0400

    Player object spawns center, and rotates

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
     clock= pygame.time.Clock()
     dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = (SCREEN_WIDTH/2)
+    y = (SCREEN_HEIGHT/2)
+    p1= Player(x,y,PLAYER_RADIUS)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,8 +17,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        dt = clock.tick(60)/1000
+        p1.draw(screen)
+        p1.update(dt) 
         pygame.display.flip()
         clock.tick(60)
-                      
+                  
 if __name__ == "__main__":
     main()
